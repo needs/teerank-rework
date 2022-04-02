33cd9db431afdcae252f0580ba3007419e7195c9
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-04-02 14:03:05 +0200

    Make sure default master servers are always in the database.

--- database/sources/rpc/all_servers.py
+++ database/sources/rpc/all_servers.py
@@ -1,16 +1,63 @@
 """Implement "all_servers" RPC."""
 
+import datetime
+
 import database_pb2
 
+DEFAULT_MASTER_SERVERS_ADDRESS = {
+    "master1.teeworlds.com:8300",
+    "master2.teeworlds.com:8300",
+    "master3.teeworlds.com:8300",
+    "master4.teeworlds.com:8300",
+}
 
-def run(_dgraph) -> database_pb2.AllServersResponse:
+
+def add_master_servers(dgraph, addresses):
+    """Add master servers."""
+    if not addresses:
+        return
+
+    down_since = datetime.datetime.min.isoformat()
+
+    dgraph.execute(
+        """
+        mutation($master_servers: [AddMasterServerInput!]!) {
+            addMasterServer(input: $master_servers) {
+                numUids
+            }
+        }
+        """,
+        {
+            "master_servers": [
+                {
+                    "address": address,
+                    "downSince": down_since,
+                }
+                for address in addresses
+            ]
+        },
+    )
+
+
+def run(dgraph) -> database_pb2.AllServersResponse:
     """Implement "all_servers" RPC."""
+    query = dgraph.execute(
+        """
+        query {
+            queryGameServer {
+                address
+            }
+            queryMasterServer {
+                address
+            }
+        }
+        """
+    )
+
+    master_servers_address = {s["address"] for s in query["queryMasterServer"]}
+    add_master_servers(dgraph, DEFAULT_MASTER_SERVERS_ADDRESS - master_servers_address)
+
     return database_pb2.AllServersResponse(
-        game_servers_address=[],
-        master_servers_address=[
-            "master1.teeworlds.com:8300",
-            "master2.teeworlds.com:8300",
-            "master3.teeworlds.com:8300",
-            "master4.teeworlds.com:8300",
-        ],
+        game_servers_address=[s["address"] for s in query["queryGameServer"]],
+        master_servers_address=master_servers_address | DEFAULT_MASTER_SERVERS_ADDRESS,
     )
--- database/tests/test_rpc/test_all_servers.py
+++ database/tests/test_rpc/test_all_servers.py
@@ -1,16 +1,37 @@
 """Test all_servers()."""
 
 import rpc.all_servers
+from rpc.all_servers import DEFAULT_MASTER_SERVERS_ADDRESS, add_master_servers
 
 
-def test_all_servers(dgraph):
+def test_all_servers_no_servers(dgraph):
     """Test all_server() when there is no servers already stored."""
     response = rpc.all_servers.run(dgraph)
-
     assert response.game_servers_address == []
-    assert response.master_servers_address == [
-        "master1.teeworlds.com:8300",
-        "master2.teeworlds.com:8300",
-        "master3.teeworlds.com:8300",
-        "master4.teeworlds.com:8300",
-    ]
+    assert set(response.master_servers_address) == DEFAULT_MASTER_SERVERS_ADDRESS
+
+
+def test_all_servers_some_master_servers(dgraph):
+    """Test all_server() when there is some servers already stored."""
+    master_servers_address = {
+        "master-server-test1:8080",
+        "master-server-test2:8080",
+    }
+
+    add_master_servers(dgraph, list(master_servers_address))
+
+    response = rpc.all_servers.run(dgraph)
+    assert response.game_servers_address == []
+    assert (
+        set(response.master_servers_address)
+        == DEFAULT_MASTER_SERVERS_ADDRESS | master_servers_address
+    )
+
+
+def test_all_servers_some_default_master_servers(dgraph):
+    """Test all_server() when there is some default servers already stored."""
+    add_master_servers(dgraph, list(DEFAULT_MASTER_SERVERS_ADDRESS))
+
+    response = rpc.all_servers.run(dgraph)
+    assert response.game_servers_address == []
+    assert set(response.master_servers_address) == DEFAULT_MASTER_SERVERS_ADDRESS
