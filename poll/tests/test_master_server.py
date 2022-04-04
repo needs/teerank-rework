903ca58f81ee92015461724d3e57ada5eddd5c4b
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-04-04 14:49:53 +0200

    Test master server start_polling().

---
+++ poll/tests/test_master_server.py
@@ -0,0 +1,18 @@
+"""Test master_server.py."""
+
+from master_server import MasterServer
+from packet import Packet
+
+
+def test_master_server_start_polling():
+    """Test start_polling()."""
+    master_server = MasterServer("master1.teeworlds.com:8300", None)
+
+    packets = master_server.start_polling()
+
+    expected_packet = Packet()
+    expected_packet.pack_bytes(bytearray(b"\xff" * 10))
+    expected_packet.pack_bytes(bytearray(b"req2"))
+
+    assert len(packets) == 1
+    assert bytes(packets[0]) == bytes(expected_packet)
