22656ac4d266d87cb460e03f5a48bc5c28056e96
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-04-04 18:31:44 +0200

    Test when master server answers with one IPv6 server.

--- poll/tests/test_master_server.py
+++ poll/tests/test_master_server.py
@@ -1,6 +1,6 @@
 """Test master_server.py."""
 
-from socket import AF_INET, inet_pton
+from socket import AF_INET, AF_INET6, inet_pton
 
 import pytest
 from master_server import MasterServer
@@ -87,8 +87,8 @@ def test_master_server_no_server(master_server, server_pool, update_stub):
     assert update_stub.up_requests[0].address == master_server.address
 
 
-def test_master_server_one_server(master_server, server_pool, update_stub):
-    """Test when master server answer with one server."""
+def test_master_server_one_server_ipv4(master_server, server_pool, update_stub):
+    """Test when master server answer with one IPv4 server."""
     host = "127.0.0.255"
     port = 40211
 
@@ -107,6 +107,31 @@ def test_master_server_one_server(master_server, server_pool, update_stub):
 
     assert len(server_pool.servers) == 1
     assert server_pool.servers[0].address == f"{host}:{port}"
+
+    assert len(update_stub.down_requests) == 0
+    assert len(update_stub.up_requests) == 1
+    assert update_stub.up_requests[0].address == master_server.address
+
+
+def test_master_server_one_server_ipv6(master_server, server_pool, update_stub):
+    """Test when master server answer with one IPv6 server."""
+    host = "16e8:5e3a:e56e:b67c:17c:1561:6e5b:d116"
+    port = 40211
+
+    master_server.start_polling()
+
+    packet = Packet()
+    packet.pack_bytes(bytearray(b"\x00" * 10))
+    packet.pack_bytes(bytearray(b"lis2"))
+
+    packet.pack_bytes(inet_pton(AF_INET6, host))
+    packet.pack_bytes(port.to_bytes(2, byteorder="big"))
+
+    master_server.process_packet(packet)
+    master_server.stop_polling(update_stub, None)
+
+    assert len(server_pool.servers) == 1
+    assert server_pool.servers[0].address == f"[{host}]:{port}"
 
     assert len(update_stub.down_requests) == 0
     assert len(update_stub.up_requests) == 1
