795b8c51314f0df3a6160290922db31717a4f755
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-04-05 13:31:36 +0200

    is_up(): Check that clients are updated and ranked.

--- poll/tests/test_game_server.py
+++ poll/tests/test_game_server.py
@@ -24,6 +24,42 @@ def game_info_packet_header(game_server: GameServer, packet_type: bytes) -> Pack
     return packet
 
 
+def game_info_extended_packet(game_server: GameServer, num_clients: int):
+    """Create a game info packet with the extended format."""
+    packet = game_info_packet_header(game_server, b"iext")
+
+    packet.pack("0.0.6")
+    packet.pack("test-server")
+    packet.pack("test-map")
+    packet.pack_int(0)  # Map CRC.
+    packet.pack_int(0)  # Map size.
+    packet.pack("test-gametype")
+    packet.pack_int(0)  # Flags
+    packet.pack_int(num_clients)  # Number of players.
+    packet.pack_int(16)  # Maximum number of players.
+    packet.pack_int(num_clients)  # Number of clients.
+    packet.pack_int(16)  # Maximum number of clients.
+
+    packet.pack("reserved")  # Extra data.
+
+    return packet
+
+
+def pack_client(packet: Packet, client: dict) -> None:
+    """Pack the given client in the packet with the vanilla format."""
+    packet.pack(client["name"])
+    packet.pack(client["clan"])
+    packet.pack_int(client["country"])  # Country
+    packet.pack_int(client["score"])  # Score
+    packet.pack_int(client["ingame"])  # Ingame
+
+
+def pack_client_extended(packet: Packet, client: dict) -> None:
+    """Pack the given client in the packet with the extended format."""
+    pack_client(packet, client)
+    packet.pack("reserved")
+
+
 @pytest.fixture(name="client1")
 def fixture_client1():
     """Return game server client."""
@@ -56,15 +92,22 @@ def fixture_game_server():
 
 @pytest.fixture(name="is_up")
 def fixture_is_up(game_server, rank_stub, update_stub):
-    """Return a function checking if the game server is up."""
+    """Return a function checking if the game server is up with the given clients."""
 
-    def is_up():
+    def is_up(clients: list[dict] = []):
+        updated_names = {
+            client.name for client in update_stub.game_up_requests[0].clients
+        }
+        ranked_names = {client.name for client in rank_stub.rank_requests[0].clients}
+        client_names = {client["name"] for client in clients}
+
         return (
             len(update_stub.game_up_requests) == 1
             and len(update_stub.game_down_requests) == 0
             and update_stub.game_up_requests[0].address == game_server.address
             and len(rank_stub.rank_requests) == 1
             and rank_stub.rank_requests[0].address == game_server.address
+            and updated_names.intersection(ranked_names) == client_names
         )
 
     return is_up
@@ -79,7 +122,7 @@ def test_game_server_start_polling(game_server):
     assert packet_type(packets[1]) == b"fstd"
 
 
-def test_game_server_vanilla(game_server, update_stub, rank_stub, is_up):
+def test_game_server_vanilla(game_server, update_stub, rank_stub, client1, is_up):
     """Test sending a vanilla packet."""
     game_server.start_polling()
 
@@ -95,19 +138,15 @@ def test_game_server_vanilla(game_server, update_stub, rank_stub, is_up):
     packet.pack_int(1)  # Number of clients.
     packet.pack_int(16)  # Maximum number of clients.
 
-    packet.pack("test-player-1")
-    packet.pack("test-clan-1")
-    packet.pack_int(1)  # Country
-    packet.pack_int(2)  # Score
-    packet.pack_int(1)  # Ingame
+    pack_client(packet, client1)
 
     game_server.process_packet(packet)
     game_server.stop_polling(update_stub, rank_stub)
 
-    assert is_up()
+    assert is_up([client1])
 
 
-def test_game_server_legacy_64(game_server, update_stub, rank_stub, is_up):
+def test_game_server_legacy_64(game_server, update_stub, rank_stub, client1, is_up):
     """Test sending a legacy 64 packet."""
     game_server.start_polling()
 
@@ -125,47 +164,12 @@ def test_game_server_legacy_64(game_server, update_stub, rank_stub, is_up):
 
     packet.pack_bytes(b"\x00")  # Extra data.
 
-    packet.pack("test-player-1")
-    packet.pack("test-clan-1")
-    packet.pack_int(1)  # Country
-    packet.pack_int(2)  # Score
-    packet.pack_int(1)  # Ingame
+    pack_client(packet, client1)
 
     game_server.process_packet(packet)
     game_server.stop_polling(update_stub, rank_stub)
 
-    assert is_up()
-
-
-def pack_client_extended(packet: Packet, client: dict) -> None:
-    """Pack the given client in the packet with the extended format."""
-    packet.pack(client["name"])
-    packet.pack(client["clan"])
-    packet.pack_int(client["country"])  # Country
-    packet.pack_int(client["score"])  # Score
-    packet.pack_int(client["ingame"])  # Ingame
-    packet.pack("reserved")
-
-
-def game_info_extended_packet(game_server: GameServer, num_clients: int):
-    """Create a game info packet with the extended format."""
-    packet = game_info_packet_header(game_server, b"iext")
-
-    packet.pack("0.0.6")
-    packet.pack("test-server")
-    packet.pack("test-map")
-    packet.pack_int(0)  # Map CRC.
-    packet.pack_int(0)  # Map size.
-    packet.pack("test-gametype")
-    packet.pack_int(0)  # Flags
-    packet.pack_int(num_clients)  # Number of players.
-    packet.pack_int(16)  # Maximum number of players.
-    packet.pack_int(num_clients)  # Number of clients.
-    packet.pack_int(16)  # Maximum number of clients.
-
-    packet.pack("reserved")  # Extra data.
-
-    return packet
+    assert is_up([client1])
 
 
 def test_game_server_extended(game_server, update_stub, rank_stub, client1, is_up):
@@ -178,10 +182,7 @@ def test_game_server_extended(game_server, update_stub, rank_stub, client1, is_u
     game_server.process_packet(packet)
     game_server.stop_polling(update_stub, rank_stub)
 
-    assert is_up()
-
-    assert len(update_stub.game_up_requests[0].clients) == 1
-    assert update_stub.game_up_requests[0].clients[0].name == client1["name"]
+    assert is_up([client1])
 
 
 def test_game_server_extended_more(
@@ -203,12 +204,4 @@ def test_game_server_extended_more(
 
     game_server.stop_polling(update_stub, rank_stub)
 
-    assert is_up()
-
-    assert len(update_stub.game_up_requests[0].clients) == 2
-    assert update_stub.game_up_requests[0].clients[0].name == client1["name"]
-    assert update_stub.game_up_requests[0].clients[1].name == client2["name"]
-
-    assert len(rank_stub.rank_requests[0].clients) == 2
-    assert rank_stub.rank_requests[0].clients[0].name == client1["name"]
-    assert rank_stub.rank_requests[0].clients[1].name == client2["name"]
+    assert is_up([client1, client2])
