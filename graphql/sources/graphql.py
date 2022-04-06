2816768cb066e6d3d4934515f73b5238b92b285c
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-04-06 13:28:46 +0200

    Fix graphql mitmproxy addon not working.

--- graphql/sources/graphql.py
+++ graphql/sources/graphql.py
@@ -1,15 +1,17 @@
 """Intercept all traffic to /graphql."""
 
+import json
+
 
 def _explore(value, on_string):
     """Explore value and call on_string on all instance of str."""
     if isinstance(value, dict):
         for key, val in value.items():
-            value[key] = _explore(val, transform)
+            value[key] = _explore(val, on_string)
 
     elif isinstance(value, list):
         for i, val in enumerate(value):
-            value[i] = _explore(val, transform)
+            value[i] = _explore(val, on_string)
 
     elif isinstance(value, str):
         value = on_string(value)
@@ -24,7 +26,7 @@ def _serialize_string(string):
 
 def _unserialize_string(string):
     """Cancel the effect of serialize_string()."""
-    return json.loads(f'"{value}"')
+    return json.loads(f'"{string}"')
 
 
 def request(flow):
@@ -35,7 +37,7 @@ def request(flow):
         content = json.loads(flow.request.content)
         if "variables" in content:
             content["variables"] = _explore(content["variables"], _serialize_string)
-            flow.request.content = json.dumps(content)
+            flow.request.content = json.dumps(content).encode()
 
 
 def response(flow):
@@ -44,4 +46,4 @@ def response(flow):
         content = json.loads(flow.response.content)
         if "data" in content:
             content["data"] = _explore(content["data"], _unserialize_string)
-            flow.response.content = json.dumps(content)
+            flow.response.content = json.dumps(content).encode()
